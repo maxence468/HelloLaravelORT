fa34fca320c560e66c94aae863bb905ca94e3072
maxence468 <maxencebarth12@gmail.com>
2025-09-25 17:00:41 +0200

    intro laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Bienvenue dans le home</h1>
+    <p> {{$greeting}} {{$nom_utilisateur }} </p>
+    <p>{{$a}}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    $username = 'Jean-Pierre';
+    $greeting = 'Yo ';
+
+    return view('home',[
+        'nom_utilisateur'=>$username,
+        'greeting'=>$greeting
+    ]);
+});
+
